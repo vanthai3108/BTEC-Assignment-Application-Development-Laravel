64fdcf9db3b0b470248cb3bf0cd238a3e588dabb
Your Name <tudmbhaf190149@fpt.edu.vn>
2021-11-10 15:52:48 +0700

    edit topic

---
+++ resources/views/admin/topic/create.blade.php
@@ -0,0 +1,47 @@
+@extends('adminlte::page')
+
+@section('title', 'Admin | Create topic')
+
+@section('content_header')
+    <h1>Create new topic</h1>
+@stop
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col col-6">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Create new topic</h3>
+                      </div>
+                    <form action="{{route('admin.topics.store')}}" method="POST">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="topicName">Name:</label>
+                                <input type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" 
+                                    id="topicName" name="name" value="{{ old('name') }}" placeholder="Topic name">
+                                @if ($errors->has('name'))
+                                    <div class="invalid-feedback">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary col col-12">Save</button>
+                        </div>
+                    </form>
+                </div>                
+            </div>
+        </div>        
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
---
+++ resources/views/admin/topic/list.blade.php
@@ -0,0 +1,57 @@
+@extends('adminlte::page')
+
+@section('title', 'Admin | List of topics')
+
+@section('content_header')
+    <h1>List of topics</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"><a href="{{ route('admin.topics.create') }}" class="text-green"><i class="fas fa-plus text-green"></i> Create new topic</a></h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th class="text-center">#</th>
+                        <th class="text-center">Name</th>
+                        <th colspan="2" class="text-center">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($topics as $topic)
+                        <tr>
+                            <td class="text-center align-middle">{{ ($topics->currentPage() - 1)  * $topics->perpage() + $loop->iteration }}</td>
+                            <td class="align-middle">{{ $topic->name }}</td>
+                            <td class="text-center align-middle"><a href="{{ route('admin.topics.edit', $topic->id) }}"><i class="fas fa-edit text-blue"></i></a></td>
+                            <td class="align-middle">
+                                <form action="{{ route('admin.topics.destroy',$topic->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-block"><i class="fas fa-trash text-red"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer clearfix">
+            <ul class="pagination pagination-sm m-0 justify-content-center">
+                {{$topics->links('vendor.pagination.custom')}}
+            </ul>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
